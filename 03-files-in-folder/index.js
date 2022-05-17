f55f48d601f476522b390267487ae1648029d9a4
Ulukbek Kasymov <ulik.kasymoff@gmail.com>
2022-05-18 01:23:55 +0600

    feat: solve third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, (err, files) => { 
+  if (err) console.log(err);
+  files.forEach((file) => {
+    const currentPath = path.join(folderPath, file);
+    fs.stat(currentPath, (err, stats) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (stats.isFile()) console.log(path.parse(file).name + ' - ' +path.extname(file) + ' - ' + stats.size/1000 + 'kB');
+      }
+    });
+  });
+});
