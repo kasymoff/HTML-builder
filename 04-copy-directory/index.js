4f764cdcc607c12cf5902d85a7bbd0bbef7d660f
Ulukbek Kasymov <ulik.kasymoff@gmail.com>
2022-05-18 11:44:32 +0600

    feat: solve fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const path = require('path');
 
+const folderPath = path.join(__dirname, 'files');
+const copyFolderPath = path.join(__dirname, 'files-copy');
+
+fs.mkdir(copyFolderPath, err => {
+  if (err) null;
+});
+
+fs.readdir(copyFolderPath, (err, files) => {
+  if (err) null;
+  files.map(file => fs.unlink(path.join(copyFolderPath, file), err => err ? null : null));
+});
+
+fs.readdir(folderPath, (err, files) => {
+  if (err) null;
+  files.map(file => {
+    fs.copyFile(path.join(folderPath, file), path.join(copyFolderPath, file), err => {
+      if (err) null;
+    });
+  });
+});
